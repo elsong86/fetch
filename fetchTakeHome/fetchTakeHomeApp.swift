5ec7e12bde4258592a099604b87467473b59d9b5
elsong86 <ellissong@gmail.com>
2025-03-18 11:36:56 -0700

    Initial Commit

---
+++ fetchTakeHome/fetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchTakeHomeApp.swift
+//  fetchTakeHome
+//
+//  Created by Ellis Song on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct fetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fetchTakeHomeTests/fetchTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fetchTakeHomeTests.swift
+//  fetchTakeHomeTests
+//
+//  Created by Ellis Song on 3/18/25.
+//
+
+import Testing
+@testable import fetchTakeHome
+
+struct fetchTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
